5195ce94eac4d4775ee0763c44ae26e71b80e56b
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-03-11 12:10:58 +0100

    Riktig formattering

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ import {spawn} from 'child_process';
 
 const app = express()
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
- 
- let dataToSend;
- const python = spawn('python', ['lunsj.py']);
- 
- python.stdout.on('data', function (data) {
-  dataToSend = data.toString();
- });
 
- python.on('close', (code) => {
-    res.send(dataToSend);
- });
+app.get('/', (req, res) => {
+    let dataToSend;
+    const python = spawn('python', ['lunsj.py'], );
+    res.set({ 'content-type': 'text/plain; charset=utf-8' });
+
+    python.stdout.on('data', function (data) {
+        const buffer = Buffer.from(data);
+
+        dataToSend = buffer.toString('latin1');
+    });
+
+    python.on('close', (code) => {
+        res.send(`${dataToSend}`);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
