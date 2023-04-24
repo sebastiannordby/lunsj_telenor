188fe5540c2eb942fc451bd275389f42374225b6
marbraa <126198866+marbraa@users.noreply.github.com>
2023-04-24 12:24:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ function getPage(req, res, day) {
         <!DOCTYPE html>
         <html lang="en">
         <head>
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZB3ZXQX08B"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-ZB3ZXQX08B');
+            </script>
             <title>Lunsjmeny Fornebu</title>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
