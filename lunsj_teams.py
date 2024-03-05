c529f33d445d82d85f954882918c61be5d46ded9
marbraa <126198866+marbraa@users.noreply.github.com>
2024-03-05 15:17:45 +0100

    Update lunsj_teams.py

--- lunsj_teams.py
+++ lunsj_teams.py
@@ -100,9 +100,8 @@ if __name__ == "__main__":
     if dag > 4:
         print("\nIngen meny på lørdag og søndag. Kom tilbake på mandag :)")
     else:
-        print("God morgen! \n")
-        print("\U0001f4c6", ukedag + " " + today.strftime("%d.%m %Y") + " - Uke " + "" + "\n")
-        print("\U0001f30d Fornebu - Bærum - Viken \U0001f556 07:00 - _VÆRET KOMMER HER_\n")
+        print("\U0001f4c6", ukedag + " " + today.strftime("%d.%m %Y")")
+        print("\U0001f30d Fornebu - Bærum - Viken \U0001f556 07:00\n")
         print("\U0001f37d Lunsj -og middagsmeny\n")
         for c in canteens:
             y, v = get_menu(c, dag)
