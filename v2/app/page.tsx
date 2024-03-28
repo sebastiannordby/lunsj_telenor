2bb32e04e13e23da96af997c23b4e7e742a6113f
Sebastian Nordby <sebastianbjornstad@hotmail.com>
2024-03-28 12:58:41 +0100

    v2: tada

--- v2/app/page.tsx
+++ v2/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { API } from '@/lib/api';
-import { Canteen, CanteenMenu, CanteenView } from '@/lib/definitions';
-import { Card, CardBody, CardFooter, CardHeader, Divider, Select, SelectItem } from '@nextui-org/react';
+import { CanteenMenu, CanteenView } from '@/lib/definitions';
+import { Card, CardBody, CardHeader, Divider, Select, SelectItem, Skeleton } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 
 // Define the types for your menu data
@@ -29,30 +29,24 @@ const DAYS = {
     "7": "Søndag"
 } as any;
 
-function fetchMenuForDay<Menu>(day: string) {
-  return {
-    "Kantine 1": [ "Kylling", "Taco", "Burger" ],
-    "Kantine 2": [ "Kylling", "Taco", "Burger" ],
-    "Kantine 3": [ "Kylling", "Taco", "Burger" ],
-  };
-}
+const getDayName = () => {
+  let day = new Date().getDay();
+
+  if(day == 0) {
+    day = 7;
+  }
+
+  const dayName = (DAYS as any)[day];
+
+  console.log('getDay: ', dayName);
+
+  return dayName as string;
+};
 
 export default function Home() {
-  const [selectedDay, setSelectedDay] = useState<string>('Mandag');
-  const [menu, setMenu] = useState<Menu>({});
+  const [selectedDay, setSelectedDay] = useState<string>('');
+  const [menu, setMenu] = useState<Menu | null>(null);
   const [canteens, setCanteens] = useState<CanteenView[]>();
-
-  useEffect(() => {
-    let day = new Date().getDay();
-
-    if(day == 0) {
-      day = 7;
-    }
-
-    const dayName = (DAYS as any)[day];
-
-    setSelectedDay(dayName as string);
-  }, []);
 
   const handleDayChange = async(event: React.ChangeEvent<HTMLSelectElement>) => {
     const newDay = event.target.value;
@@ -64,15 +58,19 @@ export default function Home() {
   };
 
   useEffect(() => {
+    setSelectedDay(getDayName());
+  }, []);
+
+  useEffect(() => {
     (async() => {
       await fetchCanteens();
     })();
   }, []);
 
-  useEffect(() => {
+  const loadMenus = async(dayToSelect: string) => {
     const menu: Menu = {};
     const dayEntries = Object.entries(DAYS);
-    const dayEntry = dayEntries.find(x => x[1] == selectedDay);
+    const dayEntry = dayEntries.find(x => x[1] == dayToSelect);
     const dayAsNumber = dayEntry?.[0] ?? 1;
 
     canteens?.forEach(x => {
@@ -81,49 +79,81 @@ export default function Home() {
     });
 
     setMenu(menu);
+  };
 
-  }, [ selectedDay ]);
+  useEffect(() => {
+    (async() => {
+      if(selectedDay && (canteens?.length ?? 0) >= 1) {
+        await loadMenus(selectedDay);
+      }
+    })();
+  }, [ selectedDay, canteens ]);
 
-  return (
-    <div className="flex-1 container mx-auto p-8 rounded-lg overflow-auto">
-      <h1 className="text-center mb-6 text-2xl font-bold text-white md:text-3xl">Lunsjmeny - Fornebu</h1>
+
+  if(menu !== null) {
+    return(
+       <div className="flex-1 container mx-auto p-4 rounded-lg overflow-auto">
+         <h1 className="text-center mb-6 mt-4 text-4xl font-bold text-white md:text-3xl">Lunsjmeny Fornebu</h1>
+         
+         <div className='flex flex-col gap-2 justify-center items-center flex-1'>
+           <Select
+             key={selectedDay}
+             label="Viser meny for"
+             placeholder="Velg dag for å vise meny"
+             className="max-w-xs"
+             size='lg'
+             fullWidth={true}
+             value={selectedDay}
+             defaultSelectedKeys={[selectedDay]}
+             selectionMode='single'
+             onChange={handleDayChange}>
+             {daysOfWeek.map((day) => (
+               <SelectItem key={day} value={day}>
+                 {day}
+               </SelectItem>
+             ))}
+           </Select>
+   
+           <div className='p-2 flex flex-col gap-2 overflow-auto w-full items-center'>
+             {Object.entries(menu).map(([canteenName, canteenMenu]) => (
+               <Card className="max-w-[400px]" key={canteenName} fullWidth={true}>
+                 <CardHeader className="flex gap-3">
+                   <div className="flex flex-col">
+                     <p className="text-md">{canteenName}</p>
+                   </div>
+                 </CardHeader>
+                 <Divider/>
+                 <CardBody>
+                   <div className="pl-5">
+                     {canteenMenu.map((menu, index) => (
+                       <p key={index} dangerouslySetInnerHTML={{ __html: menu.description.replace(/\n/g, '<br/>') }} />
+                     ))}
+                   </div>                
+                 </CardBody>
+               </Card>          
+             ))}
+           </div>
+         </div>
+       </div>
+     );
+  } else {
+    return (
+      <div className="flex-1 container mx-auto p-4 rounded-lg overflow-auto min-w-full w-full">
+      <h1 className="text-center mb-6 mt-4 text-4xl font-bold text-white md:text-3xl">Lunsjmeny Fornebu</h1>
       
-      <div className='flex flex-col gap-2 justify-center items-center flex-1'>
-        <Select
-          label="Viser meny for"
-          placeholder="Velg dag for å vise meny"
-          className="max-w-xs"
-          size='lg'
-          fullWidth={true}
-          value={selectedDay}
-          onChange={handleDayChange}>
-          {daysOfWeek.map((day) => (
-            <SelectItem key={day} value={day}>
-              {day}
-            </SelectItem>
-          ))}
-        </Select>
-
-        <div className='p-2 flex flex-col gap-2 overflow-auto w-full items-center'>
-          {Object.entries(menu).map(([canteenName, canteenMenu]) => (
-            <Card className="max-w-[400px]" key={canteenName} fullWidth={true}>
-              <CardHeader className="flex gap-3">
-                <div className="flex flex-col">
-                  <p className="text-md">{canteenName}</p>
-                </div>
-              </CardHeader>
-              <Divider/>
-              <CardBody>
-                <div className="pl-5">
-                  {canteenMenu.map((menu, index) => (
-                    <p key={index} dangerouslySetInnerHTML={{ __html: menu.description.replace(/\n/g, '<br/>') }} />
-                  ))}
-                </div>                
-              </CardBody>
-            </Card>          
-          ))}
+      <div className='flex flex-col gap-2 justify-center flex-1 w-full items-center'>
+      <div className="spinner-box">
+        <div className="spinner-box">
+          <div className="configure-border-1">  
+            <div className="configure-core"></div>
+          </div>  
+          <div className="configure-border-2">
+            <div className="configure-core"></div>
+          </div> 
+        </div>    
         </div>
-      </div>
+      </div>      
     </div>
-  );
+    );
+  }
 };
