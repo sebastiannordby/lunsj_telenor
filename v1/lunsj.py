912f5b1bd95902d26bb7984ee78d3572fc0b5da7
marbraa <126198866+marbraa@users.noreply.github.com>
2024-04-04 22:27:10 +0200

    Update lunsj.py

--- v1/lunsj.py
+++ v1/lunsj.py
@@ -35,18 +35,18 @@ def print_menu_for_day(day: int):
             canteen_name = filename.split(" - ", 1)[1][:-4]  # Henter kantinens navn fra filnavnet
             with open(os.path.join("Menyer/", filename), "r") as file:
                 menu_content = file.readlines()[1:]  # Fjerner den første linjen
-                menu_content = "".join(menu_content)
+                menu_content = "".join(line for line in menu_content if line.strip())  # Fjerner tomme linjer
                 if "Middag" in filename:
                     middag_menus.append((canteen_name, menu_content))
                 else:
                     non_middag_menus.append((canteen_name, menu_content))
 
     for menu in non_middag_menus:
-        print(f"{menu[0]}: (10:30 - 13:00)")
+        print(f"\n{menu[0]}: (10:30 - 13:00)")
         print(menu[1])
 
     for menu in middag_menus:
-        print(f"{menu[0]} (15:00 - 17:00):")
+        print(f"\n{menu[0]} (15:00 - 17:00):")
         print(menu[1])
 
 if __name__ == "__main__":
